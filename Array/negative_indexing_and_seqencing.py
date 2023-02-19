7e538c779b0f4e1598d06c8f98523a01d122129c
mdtalha178 <talhaazmi178@gmail.com>
2023-02-20 00:24:26 +0530

    negative indexing

---
+++ Array/negative_indexing_and_seqencing.py
@@ -0,0 +1,31 @@
+# defing a list 
+li = [1,2,3,4]
+
+# postive index
+"""Access an with postive index.
+   when we access an element with postive index it will give an element from starting
+"""
+print(li[1], "Access an element with positive postive index")
+
+# negative index
+"""Access an element with negative negative index
+   when we access an element with negative index it will give an element from ending
+"""
+print(li[-1], "Access an element with negative index")
+
+# sequencing
+"""There is various ways to access an element from a list
+   it works with this structure:
+   list = [start_index, end_index, step]
+"""
+print(li[1:],end= " ")
+print("it will all element from index 1 to last index")
+
+
+"""it will give you all elements from index 1 to last index
+"""
+print(li[1:4:1])
+
+"""it will give you all elements from index 1 to last index but skip every next elemnt
+"""
+print(li[1:4:2])
