8e6ac476c9aef6e8dc83e8794a68d4f42f9cdd1b
mdtalha178 <talhaazmi178@gmail.com>
2023-03-12 23:19:49 +0530

    Problem Added on 2 Dimensional Array

--- Two_Dimenionsal/input_of_2d_list.py
+++ Two_Dimenionsal/input_of_2d_list.py
@@ -4,8 +4,7 @@ In this we learn how take input of 2d list
 # Problem Statement 1.1:
 """
 How to take input of 2d list
-Note:
-This jagged list because we not restricting for the number of column
+
 """
 # How many rows and column
 number_of_rows_column = input().split()
